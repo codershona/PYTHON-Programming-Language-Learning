594e889267de1b2cbd9d7a1ee8ceaa0b0794f35d
codershona <falgunimst95@gmail.com>
2020-01-27 22:10:25 +0100

    working on python dictionaries

---
+++ Python Dictionaries/Python Dictionaries.py
@@ -0,0 +1,24 @@
+# Python Dictionaries
+# Dictionary
+# A dictionary is a collection which is unordered, changeable and indexed. In Python dictionaries are written with curly brackets, and they have keys and values.
+
+# Example
+# Create and print a dictionary:
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict)
+
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+
+
